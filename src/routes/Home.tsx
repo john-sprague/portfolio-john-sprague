ca1790693cd7b82ad9f120ea9637017c209c3c47
John Sprague <spraguejdev@gmail.com>
2024-12-26 21:51:48 -0500

    chore: add react router, define routes, add new pages

---
+++ src/routes/Home.tsx
@@ -0,0 +1,27 @@
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+      <nav className="my-16 animate-bounce">
+        <ul className="flex items-center justify-center gap-5"></ul>
+      </nav>
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+
+      <h1 className=" p-10 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
+        John Sprague
+      </h1>
+
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="my-16 text-center animate-fade-in">
+        <h2 className="text-md text-zinc-500 ">
+          Hi, my name is John, I'm an Enterprise Software Engineer with
+          knowledge and experience in front-end / full stack development,
+          <br />
+          UX, and complex problem solving, producing code and features that
+          strengthened applications for IBM.
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
