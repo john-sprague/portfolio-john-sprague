d913cbc0f9eac62f35bf25ed9d1995d0b413d077
John Sprague <spraguejdev@gmail.com>
2024-12-26 21:59:50 -0500

    chore: add the link component to navigate to pages

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,8 +1,31 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
+  const navigation = [
+    {
+      to: "projects",
+      name: "Projects",
+    },
+    {
+      to: "contact",
+      name: "Contact",
+    },
+  ];
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
       <nav className="my-16 animate-bounce">
-        <ul className="flex items-center justify-center gap-5"></ul>
+        <ul className="flex items-center justify-center gap-5">
+          {navigation.map((item) => {
+            return (
+              <Link
+                className="text-md duration-500 text-zinc-500 hover:text-zinc-300"
+                to={item.to}
+              >
+                {item.name}
+              </Link>
+            );
+          })}
+        </ul>
       </nav>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 
