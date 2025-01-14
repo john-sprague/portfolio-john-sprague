551cff567a1571649d48e9162c075c38ee28dec7
John Sprague <spraguejdev@gmail.com>
2025-01-13 20:44:27 -0500

    chore: add initial styles to Main Navigation

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom"
 const MainNavigation = () => {
     return (
         <header>
-            <nav>
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/projects'>Projects</Link></li>
-                    <li><Link to='/contact'>Contact</Link></li>
+            <nav className="my-16 animate-bounce">
+                <ul className="flex items-center justify-center gap-5">
+                    <li><Link to='/'  className="text-md duration-500 text-zinc-500 hover:text-zinc-300">Home</Link></li>
+                    <li><Link to='/projects'  className="text-md duration-500 text-zinc-500 hover:text-zinc-300">Projects</Link></li>
+                    <li><Link to='/contact'  className="text-md duration-500 text-zinc-500 hover:text-zinc-300">Contact</Link></li>
                 </ul>
             </nav>
         </header>
