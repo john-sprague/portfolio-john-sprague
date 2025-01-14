786b90650de2824b10016e4d1e98689d37424840
John Sprague <spraguejdev@gmail.com>
2025-01-13 20:41:46 -0500

    chore: add RootLayoutComponent and add to APP.js

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,26 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/Home";
 import Projects from "./routes/Projects";
 import Contact from "./routes/Contact";
+import RootLayout from "./routes/RootLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/projects",
-    element: <Projects />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/projects",
+        element: <Projects />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
