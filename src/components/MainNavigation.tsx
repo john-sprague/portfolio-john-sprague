6ef18e47a7fcdfec96480286089af1aedfa3f693
John Sprague <spraguejdev@gmail.com>
2025-01-13 20:41:18 -0500

    chore: add a MainNavigation component

---
+++ src/components/MainNavigation.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+
+const MainNavigation = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/projects'>Projects</Link></li>
+                    <li><Link to='/contact'>Contact</Link></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default MainNavigation
