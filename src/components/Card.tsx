64ba26fb2a9ff371221ea09581b929fac3f855b6
John Sprague <spraguejdev@gmail.com>
2025-01-15 21:50:56 -0500

    chore: add card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { PropsWithChildren } from "react";
+
+export const Card: React.FC<PropsWithChildren> = ({ children }) => {
+	return (
+		<div className="overflow-hidden relative duration-700 border rounded-xl hover:bg-zinc-800/10 group md:gap-8 hover:border-zinc-400/50 border-zinc-600 ">
+			<div className="pointer-events-none">
+				<div className="absolute inset-0 z-0  transition duration-1000 [mask-image:linear-gradient(black,transparent)]" />
+			</div>
+			{children}
+		</div>
+	);
+};
